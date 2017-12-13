c05964400b715ce66e63d90d407d24ec97dbacf7
himself65 <761282619@qq.com>
2017-12-13 13:34:50 +0800

    单调队列的实现

---
+++ self-use/priority_deque.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+deque<int> q, m;
+void push(const int &x) {
+    q.push_back(x);
+    while (!m.empty() && m.back() < x) m.pop_back();
+    m.push_back(x);
+}
+
+void pop() {
+    int x = q.front();
+    q.pop_front();
+    if (x == m.front())
+        m.pop_front();
+}
+
+int size(){
+    return q.size();
+}
+
+int top() {
+    return m.front();
+}
