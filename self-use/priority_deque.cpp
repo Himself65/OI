f846e2bf7104b1a516f58fe10065f31576d35a56
Himself65 <761282619@qq.com>
2017-12-16 10:03:57 +0800

    update

--- self-use/priority_deque.cpp
+++ self-use/priority_deque.cpp
@@ -2,24 +2,31 @@
 #include<deque>
 using namespace std;
 
-deque<int> q, m;
-void push(const int &x) {
-    q.push_back(x);
-    while (!m.empty() && m.back() < x) m.pop_back();
-    m.push_back(x);
-}
+struct Maxqueue {
+    deque<int> q, m;
+    void push(const int &x) {
+        q.push_back(x);
+        while (!m.empty() && m.back() < x) m.pop_back();
+        m.push_back(x);
+    }
 
-void pop() {
-    int x = q.front();
-    q.pop_front();
-    if (x == m.front())
-        m.pop_front();
-}
+    void pop() {
+        int x = q.front();
+        q.pop_front();
+        if (x == m.front())
+            m.pop_front();
+    }
 
-int size(){
-    return q.size();
-}
+    int size(){
+        return q.size();
+    }
 
-int top() {
-    return m.front();
+    int top() {
+        return m.front();
+    }
+};
+
+int main() {
+    Maxqueue q;
+    return 0;
 }
