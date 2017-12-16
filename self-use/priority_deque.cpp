3db79fbc2f4381326070d99b18c685201eb886ec
himself65 <761282619@qq.com>
2017-12-16 19:53:31 +0800

    添加使用案例

--- self-use/priority_deque.cpp
+++ self-use/priority_deque.cpp
@@ -17,16 +17,30 @@ struct Maxqueue {
             m.pop_front();
     }
 
-    int size(){
+    int size() {
         return q.size();
+    }
+
+    bool empty() {
+        return q.empty();
     }
 
     int top() {
         return m.front();
     }
 };
-
+int data[] = {1, 100, 20, 15, 24, 200, 10, 5, -1};
 int main() {
     Maxqueue q;
+    int length = 3;
+    // 正确使用方法
+    for (int i = 0; data[i] != -1; i++) {
+        q.push(data[i]);
+        if (i >= length)
+            cout << q.top() << " ";
+        if (q.size() >= length) {
+            q.pop();
+        }
+    }
     return 0;
 }
