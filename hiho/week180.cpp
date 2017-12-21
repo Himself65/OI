5446274cbdd9885e1243acceadd4fc80d1346392
himself65 <761282619@qq.com>
2017-12-21 22:20:58 +0800

    update

--- hiho/week180.cpp
+++ hiho/week180.cpp
@@ -5,24 +5,21 @@ typedef long long ll;
 
 int main() {
     ll N;
-    while (cin >> N)
-    {
-        
+    while (cin >> N) {
+        ll n = N;          
+        ll k = 1;          
+        ll nums = 10;       
+        while (n - k * nums >= 0) {
+            n -= k * nums;
+            k++;
+            if (k == 2) nums *= 9;
+            else nums *= 10;
+        }
+        ll m = k == 1 ? 0 : pow(10, k - 1);
+        ll nn = n / k + m;   
+        ll mm = n % k;        
+        cout << (nn % ll(pow(10, k - mm))) 
+            / ll(pow(10, k - mm - 1)) << endl;
     }
-    // while (cin >> N) {
-    //     ll n = N;          
-    //     ll k = 1;          
-    //     ll nums = 10;       
-    //     while (n - k * nums >= 0) {
-    //         n -= k * nums;
-    //         k++;
-    //         if (k == 2) nums *= 9;
-    //         else nums *= 10;
-    //     }
-    //     ll m = k == 1 ? 0 : pow(10, k - 1);
-    //     ll nn = n / k + m;   
-    //     ll mm = n % k;        
-    //     cout << (nn % ll(pow(10, k - mm))) 
-    //         / ll(pow(10, k - mm - 1)) << endl;
-    // }
+    return 0;
 }
