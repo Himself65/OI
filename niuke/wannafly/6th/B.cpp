32055fd0a39bcfec7021abcd35069f4917830e1e
himself65 <761282619@qq.com>
2017-12-23 00:02:35 +0800

    自己写了一下

--- niuke/wannafly/6th/B.cpp
+++ niuke/wannafly/6th/B.cpp
@@ -1,53 +1,35 @@
-#include <iostream>
-#include <cstdio>
-#include <cmath>
-#include <cstring>
-#include <ctime>
-#include <cstdlib>
-#include <string>
-#include <bitset>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
-#include <algorithm>
-#include <sstream>
-#include <stack>
-#include <iomanip>
+#include<iostream>
 using namespace std;
-#define pb push_back
-#define mp make_pair
-typedef pair<int,int> pii;
-typedef long long ll;
-typedef double ld;
-typedef vector<int> vi;
-#define fi first
-#define se second
-#define fe first
-#define FO(x) {freopen(#x".in","r",stdin);freopen(#x".out","w",stdout);}
-#define Edg int M=0,fst[SZ],vb[SZ],nxt[SZ];void ad_de(int a,int b){++M;nxt[M]=fst[a];fst[a]=M;vb[M]=b;}void adde(int a,int b){ad_de(a,b);ad_de(b,a);}
-#define Edgc int M=0,fst[SZ],vb[SZ],nxt[SZ],vc[SZ];void ad_de(int a,int b,int c){++M;nxt[M]=fst[a];fst[a]=M;vb[M]=b;vc[M]=c;}void adde(int a,int b,int c){ad_de(a,b,c);ad_de(b,a,c);}
-#define es(x,e) (int e=fst[x];e;e=nxt[e])
-#define esb(x,e,b) (int e=fst[x],b=vb[e];e;e=nxt[e],b=vb[e])
-#define N 12
-ld a[N + 3], b[N + 3], c[N + 3], ex[N + 3][N + 3];
+const int maxn = 13;
+double a[maxn], b[maxn], c[maxn];
+double p[maxn];
+double dp[maxn][maxn];
 int main()
 {
-    for (int i = 1; i <= N; ++i)
-        cin >> a[i];
-    for (int i = 1; i <= N; ++i)
-        cin >> b[i];
-    for (int i = 1; i <= N; ++i)
-        cin >> c[i];
-    ex[0][0] = 1;
-    for (int i = 1; i <= N; ++i)
+    for (int i = 1; i <= 12; i++)
     {
-        ld p = (1 - a[i]) * (1 - b[i]) * (1 - c[i]);
-        for (int j = 0; j <= N; ++j)
-            ex[i][j] += ex[i - 1][j] * p,
-                ex[i][j + 1] += ex[i - 1][j] * (1 - p);
+        scanf("%lf", &a[i]);
     }
-    for (int i = 0; i <= N; ++i)
-        printf("%.6lf\n", ex[N][i]);
+    for (int i = 1; i <= 12; i++)
+    {
+        scanf("%lf", &b[i]);
+    }
+    for (int i = 1; i <= 12; i++)
+    {
+        scanf("%lf", &c[i]);
+    }
+    dp[0][0] = 1;
+    for (int i = 1; i <= 12; i++)
+    {
+        p[i] = 1 - (1 - a[i]) * (1 - b[i]) * (1 - c[i]);
+        for (int j = 0; j <= i; j++)
+        {
+            dp[i][j] = (1 - p[i]) * dp[i - 1][j] + p[i] * dp[i - 1][j - 1];
+        }
+    }
+    for (int i = 0; i <= 12; i++)
+    {
+        printf("%.6lf\n", dp[12][i]);
+    }
     return 0;
 }
