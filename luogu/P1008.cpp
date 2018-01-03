6f4c9f1982014af86cee181c6a202b93e5981ca3
himself65 <761282619@qq.com>
2018-01-03 22:18:09 +0800

    水题，枚举排列

---
+++ luogu/P1008.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+int x[10];
+void permutation1(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        x[i] = i;
+    }
+    do
+    {
+        int a = x[1] * 100 + x[2] * 10 + x[3];
+        int b = x[4] * 100 + x[5] * 10 + x[6];
+        int c = x[7] * 100 + x[8] * 10 + x[9];
+        b /= 2;
+        c /= 3;
+        if (a == b && b == c && a == c)
+            printf("%d %d %d\n", a, b * 2, c * 3);
+    } while (next_permutation(x + 1, x + 10));
+}
+
+int main()
+{
+    permutation1(9);
+    return 0;
+}
