94f8d812580adf609e53a8d4ad91c7b6ffec6891
himself65 <761282619@qq.com>
2018-01-07 00:26:53 +0800

    水

---
+++ luogu/P1047.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+typedef long long ll;
+inline ll min(ll a, ll b)
+{
+    return a < b ? a : b;
+}
+inline ll up(ll x, ll a)
+{
+    return (x / a) * a < x ? (x / a) + 1 : x / a;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll p;
+    cin >> p;
+    ll i = 3;
+    ll ans = 0x7f7f7f7f;
+    while (i--)
+    {
+        ll num, v;
+        cin >> num >> v;
+        ll t = up(p, num) * v;
+        ans = min(ans, t);
+    }
+    cout << ans;
+    return 0;
+}
