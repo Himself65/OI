2927b0812be0ba62e4be65a6a3fb02b3db840049
himself65 <761282619@qq.com>
2018-02-22 11:48:59 +0800

    重写了一遍

--- luogu/P3372.cpp
+++ luogu/P3372.cpp
@@ -1,88 +1,111 @@
-#include <algorithm>
-#include <cmath>
 #include <cstdio>
 #include <cstring>
-#include <deque>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <vector>
 using namespace std;
 #define INIT_CIN                 \
     ios::sync_with_stdio(false); \
     cin.tie(0);
-#define Left_Child(x) (x << 1)
-#define Right_Child(x) (x << 1 | 1)
-#define Mid(l, r) ((l + r) / 2)
-#define lchild rt << 1, l, m
-#define rchild rt << 1 | 1, m + 1, r
 typedef long long ll;
-typedef unsigned long long ull;
-const int INF = 0x7f7f7f7f;
-const int maxn = 1e5 + 5;
-const int maxq = 1e5 + 5;
-int N, M;
-int a[maxn];
-char T[maxq];
-int l[maxq], r[maxq], x[maxq];
-
-ll bit0[maxn], bit1[maxn];
-
-ll sum(ll *b, int i)
+#define LEFT_SON(x) (x << 1)
+#define RIGHT_SON(x) (x << 1 | 1)
+const int maxn = 100000 + 5;
+ll n, m;
+inline ll max(ll a, ll b);
+inline ll min(ll a, ll b);
+ll ans[maxn << 2], tag[maxn << 2];
+ll a[maxn];
+inline void push_up_sum(ll p)
 {
-    ll s = 0;
-    while (i > 0)
-    {
-        s += b[i];
-        i -= i & -i;
-    }
-    return s;
+    ans[p] = ans[LEFT_SON(p)] + ans[RIGHT_SON(p)];
 }
 
-void add(ll *b, int i, int v)
+inline void build(ll l, ll r, ll p)
 {
-    while (i <= N)
+    tag[p] = 0;
+    if (l == r)
     {
-        b[i] += v;
-        i += i & -i;
+        ans[p] = a[l];
+        return;
+    }
+    else
+    {
+        ll mid = (l + r) >> 1;
+        build(l, mid, LEFT_SON(p));
+        build(mid + 1, r, RIGHT_SON(p));
+        push_up_sum(p);
     }
 }
-
-inline void solve()
+inline void f(ll l, ll r, ll p, ll k)
 {
-    for (int i = 1; i <= N; i++)
-        add(bit0, i, a[i]);
-
-    for (int i = 0; i < M; i++)
+    tag[p] += k;
+    ans[p] += k * (r - l + 1);
+}
+inline void push_down(ll l, ll r, ll p)
+{
+    ll mid = (l + r) >> 1;
+    f(l, mid, LEFT_SON(p), tag[p]);
+    f(mid + 1, r, RIGHT_SON(p), tag[p]);
+    tag[p] = 0;
+}
+inline void update(ll l, ll r, ll p, ll nl, ll nr, ll k)
+{
+    if (nl <= l && r <= nr)
     {
-        if (T[i] == '1')
+        ans[p] += k * (r - l + 1);
+        tag[p] += k;
+        return;
+    }
+    push_down(l, r, p);
+    ll mid = (l + r) >> 1;
+    if (nl <= mid)
+        update(l, mid, LEFT_SON(p), nl, nr, k);
+    if (nr > mid)
+        update(mid + 1, r, RIGHT_SON(p), nl, nr, k);
+    push_up_sum(p);
+}
+inline ll query(ll l, ll r, ll p, ll qx, ll qy)
+{
+    ll res = 0;
+    if (qx <= l && r <= qy)
+        return ans[p];
+    ll mid = (l + r) >> 1;
+    push_down(l, r, p);
+    if (qx <= mid)
+        res += query(l, mid, LEFT_SON(p), qx, qy);
+    if (qy > mid)
+        res += query(mid + 1, r, RIGHT_SON(p), qx, qy);
+    return res;
+}
+int main()
+{
+    INIT_CIN;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    build(1, n, 1);
+    for (int i = 1; i <= m; i++)
+    {
+        int b, c, d, e, f, ps;
+        cin >> ps;
+        if (ps == 1)
         {
-            add(bit0, x[i], -x[i] * (l[i] - 1));
-            add(bit1, l[i], x[i]);
-            add(bit0, r[i] + 1, x[i] * r[i]);
-            add(bit1, r[i] + 1, -x[i]);
+            cin >> b >> c >> d;
+            update(1, n, 1, b, c, d);
         }
         else
         {
-            ll res = 0;
-            res += sum(bit0, r[i]) + sum(bit1, r[i]) * r[i];
-            res -= sum(bit0, l[i] - 1) + sum(bit1, l[i] - 1) * (l[i] - 1);
-            cout << res << endl;
+            cin >> e >> f;
+            cout << query(1, n, 1, e, f) << endl;
         }
     }
+    return 0;
 }
 
-int main()
+inline ll max(ll a, ll b)
 {
-    INIT_CIN; // std::cin的优化
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++)
-        cin >> a[i];
-    for (int i = 0; i < M; i++)
-    {
-    }
-    return 0;
+    return a > b ? a : b;
+}
+inline ll min(ll a, ll b)
+{
+    return a < b ? a : b;
 }
