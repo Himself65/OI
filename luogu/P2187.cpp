55197d47a407d496c5a1469875f0450e73d3faa9
himself65 <761282619@qq.com>
2018-03-07 22:39:01 +0800

    update

--- luogu/P2187.cpp
+++ luogu/P2187.cpp
@@ -1,30 +1,30 @@
-#include <algorithm>
 #include <cstdio>
-using namespace std;
 const int maxn = 100000 + 5;
 int f[maxn], g[maxn];
-int last[30], forbid[30][30];
+int last[30], cantnear[30][30];
 char b[maxn];
-int n, m;
+int n, m, ans = 0;
+inline int max(int a, int b) { return a > b ? a : b; }
 int main()
 {
     scanf("%d", &n);
-    scanf("%s", b + 1);
+    scanf("%s ", b + 1);
     for (int i = 1; i <= n; i++)
         f[i] = b[i] - 'a';
     scanf("%d", &m);
     while (m--)
     {
-        scanf("%s", b);
-        forbid[b[0] - 'a'][b[1] - 'a'] = forbid[b[1] - 'a'][b[0] - 'a'] = 1;
-    }
-    int ans = 0;
+        char x, y;
+        scanf("%c%c ", &x, &y);
+        cantnear[x - 'a'][y - 'a'] = cantnear[y - 'a'][x - 'a'] = 1;
+    } // 输入
+    // DP
     for (int i = 1; i <= n; i++)
     {
         g[i] = 1;
         for (int j = 0; j < 26; j++)
-            if (!forbid[j][f[i]])
-                g[i] = max(g[i], last[j] + 1);
+            if (!cantnear[j][f[i]]) // 如果这两个字符可以靠近
+                g[i] = max(g[i], last[j] + 1);  // 维护最大保留数
         last[f[i]] = max(last[f[i]], g[i]);
         ans = max(ans, g[i]);
     }
