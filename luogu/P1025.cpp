3dccd1f6e5027c1b98bf57a124b769ace47cbf02
Himself65 <himself6565@gmail.com>
2018-07-22 09:52:15 +0800

    毒瘤

---
+++ luogu/P1025.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+const int maxn = 10086;
+const int maxk = 110;
+#define I 0
+#define O 1
+#define A =
+#define S <
+#define SA <=
+#define MO >
+#define NO -
+#define M main
+#define P ++
+#define F(x, y, z) for (x; y; z)
+#define CI cin
+#define CO cout
+#define RE >>
+#define OU <<
+#define TW 2
+#define _ j
+#define __ i
+#define ___ int
+#define ____ n
+#define _____ k
+#define ______ ______
+#define _______ maxn
+#define _________ maxk
+#define __________ [
+#define ___________ ]
+#define IS if
+#define NOT else
+#define FUCK return
+___ __, ____, _____, ______[_______][_________];
+___ M()
+{
+    CI RE ____ RE _____;
+    F(__ A I, __ SA ____, __ P)
+    ______ __________ __ ___________ __________ O ___________ A ______ __________ __ ___________ __________ I ___________ A O;
+    F(___ __ A TW, __ SA ____, __ P)
+    F(___ _ A TW, _ SA k, _ P)
+    IS(__ MO _)
+    ______ __________ __ ___________ __________ _ ___________ A ______ __________ __ NO O ___________ __________ _ NO O ___________ + ______ __________ __ NO _ ___________ __________ _ ___________;
+    NOT
+        ______ __________ __ ___________ __________ _ ___________ A ______ __________ __ NO
+            O ___________ __________ _ NO O ___________;
+    F(___ __ A TW, __ SA ____, __ P)
+    ______ __________ O ___________ __________ k ___________ A I, ______ __________ I ___________ __________ k ___________ A I;
+    CO OU ______ __________ ____ ___________ __________ k ___________;
+    FUCK I;
+}
