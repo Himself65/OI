f5e57da965d5aa52073396ea2df3c7a343edc53f
Himself65 <himself6565@gmail.com>
2018-10-28 14:12:05 +0800

    fix lint

--- template.cpp
+++ template.cpp
@@ -1,4 +1,4 @@
-#define LOCAL // For Windows
+#define LOCAL  // For Windows
 #define TEST
 #define DATA
 #define HAND
@@ -15,9 +15,9 @@
 #include <string>
 #include <vector>
 using namespace std;
-#define INIT_CIN                 \
-    ios::sync_with_stdio(false); \
-    cin.tie(0);
+#define INIT_CIN               \
+  ios::sync_with_stdio(false); \
+  cin.tie(0);
 typedef long long ll;
 typedef unsigned long long ull;
 typedef double db;
@@ -26,43 +26,35 @@ typedef pair<int, int> pii;
 const int maxn = 1e6 + 5;
 const int INF = 0x3f3f3f3f;
 const int SUP = -0x3f3f3f3f;
-namespace BREAD
-{
+namespace BREAD {
 int n, m;
 template <class T>
-inline void read_int(T &x)
-{
-    char ch = getchar();
-    int op = 1;
-    while (ch < '0' || ch > '9')
-    {
-        if (ch == '-')
-            op = -1;
-        ch = getchar();
-    }
-    while ('0' <= ch && ch <= '9')
-        x = x * 10 + (ch - '0'), ch = getchar();
-    x *= op;
+inline void read_int(T &x) {
+  char ch = getchar();
+  int op = 1;
+  while (ch < '0' || ch > '9') {
+    if (ch == '-') op = -1;
+    ch = getchar();
+  }
+  while ('0' <= ch && ch <= '9') x = x * 10 + (ch - '0'), ch = getchar();
+  x *= op;
 }
-inline void solve()
-{
-}
-} // namespace BREAD
-int main()
-{
+inline void solve() {}
+}  // namespace BREAD
+int main() {
 #ifdef LOCAL
 #ifdef DATA
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
 #endif
 #endif
-    BREAD::solve();
+  BREAD::solve();
 #ifdef TEST
 #endif
 #ifdef LOCAL
 #ifdef HAND
-    system("pause");
+  system("pause");
 #endif
 #endif
-    return 0;
+  return 0;
 }
