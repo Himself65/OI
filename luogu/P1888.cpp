6a7f288906e35854a0d96646c94709036aa9dd46
Himself65 <himself6565@gmail.com>
2018-11-01 01:41:53 +0800

    fixed

--- luogu/P1888.cpp
+++ luogu/P1888.cpp
@@ -1,23 +1,78 @@
+#define LOCAL  // For Windows
+#define DEBUG
+// #define DATA
+#define HAND
+#include <algorithm>
+#include <cmath>
 #include <cstdio>
+#include <cstring>
+#include <deque>
 #include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
 using namespace std;
-
-inline int max(int a, int b) { return a > b ? a : b; }
-inline int min(int a, int b) { return a < b ? a : b; }
-inline int gcd(int a, int b)
-{
-    if (b == 1)
-        return a;
-    return gcd(b, a % b);
+#define INIT_CIN               \
+  ios::sync_with_stdio(false); \
+  cin.tie(0);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+const int maxn = 1e6 + 5;
+const int INF = 0x3f3f3f3f;
+const int SUP = -0x3f3f3f3f;
+namespace BREAD {
+template <class T>
+inline void read_int(T &x) {
+  char ch = getchar();
+  int op = 1;
+  while (ch < '0' || ch > '9') {
+    if (ch == '-') op = -1;
+    ch = getchar();
+  }
+  x = 0;
+  while ('0' <= ch && ch <= '9') x = x * 10 + (ch - '0'), ch = getchar();
+  x *= op;
 }
-int main()
-{
-    int a, b, c;
-    cin >> a >> b >> c;
-    // 只处理a和b
-    a = min(a, min(b, c));
-    b = max(a, max(a, b));
-    int d = gcd(a, b);
-    cout << a / d << "/" << b;
-    return 0;
+template <class T>
+inline void put_int(T x) {
+  static char _buf[1000];
+  int p = 0;
+  if (x < 0) putchar('-'), x = -x;
+  do {
+    _buf[p++] = '0' + (x % 10), x /= 10;
+  } while (x);
+  for (int i = p - 1; i >= 0; --i) putchar(_buf[i]);
+}
+inline int gcd(int a, int b) { return !b ? a : gcd(b, a % b); }
+int a, b, c;
+inline void solve() {
+  read_int(a), read_int(b), read_int(c);
+  int maxv = max(a, max(b, c)), minv = min(a, min(b, c));
+  int d = gcd(maxv, minv);
+  put_int(minv / d), putchar('/'), put_int(maxv / d);
+}
+}  // namespace BREAD
+int main() {
+#ifdef LOCAL
+#ifdef DATA
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+#endif
+#endif
+  BREAD::solve();
+#ifdef DEBUG
+#endif
+#ifdef LOCAL
+#ifdef HAND
+  system("pause");
+#endif
+#endif
+  return 0;
 }
