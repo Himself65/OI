0b9f55c0f0dd39425299e3cc488db392aa2ae202
himself65 <himself65@outlook.com>
2021-05-05 22:06:10 +0800

    leetcode: problem 4 another solution

--- leetcode/median-of-two-sorted-arrays/src/lib.rs
+++ leetcode/median-of-two-sorted-arrays/src/lib.rs
@@ -2,8 +2,30 @@ struct Solution {}
 
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-        let mut numbers: Vec<i32> = [&nums1[..], &nums2[..]].concat();
-        numbers.sort();
+        let mut numbers = vec![0; nums1.len() + nums2.len()];
+        let mut pos = 0;
+        let mut l1 = 0;
+        let mut l2 = 0;
+        while l1 < nums1.len() && l2 < nums2.len() {
+            if nums1[l1] < nums2[l2] {
+                numbers[pos] = nums1[l1];
+                l1 += 1;
+            } else {
+                numbers[pos] = nums2[l2];
+                l2 += 1;
+            }
+            pos += 1;
+        }
+        while l1 < nums1.len() {
+            numbers[pos] = nums1[l1];
+            l1 += 1;
+            pos += 1;
+        }
+        while l2 < nums2.len() {
+            numbers[pos] = nums2[l2];
+            l2 += 1;
+            pos += 1;
+        }
         let length = numbers.len();
         return if length % 2 == 1 {
             numbers[length / 2] as f64
